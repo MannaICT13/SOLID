1ef6eccdefa19f36747c4ba56d54acbbfaba566a
Md Khaled Hasan Manna <manna@sharetrip.net>
2022-12-17 22:00:12 +0600

    breaking of OCP example

--- SOLID/OpenClosedPrinciple.swift
+++ SOLID/OpenClosedPrinciple.swift
@@ -11,3 +11,30 @@ import Foundation
  Open Closed Principle:
  Open for extension But Closed for modification.
  */
+
+///Breaking of OCP
+class PaymentManager {
+    private func makeCashPayment(amount: Double) {
+        
+    }
+    
+    private func makeVisaPayment(amount: Double) {
+        
+    }
+}
+
+// When we need to update PaymentManager and need to add new feature then its break OCP.
+class _PaymentManager {
+    private func makeCashPayment(amount: Double) {
+        
+    }
+    
+    private func makeVisaPayment(amount: Double) {
+        
+    }
+    
+    // added new feature
+    private func makeMasterPayment(amount: Double) {
+        
+    }
+}
