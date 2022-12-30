e649c2b1d483f8d90dd82b7cc85f8e9713cda08a
Md Khaled Hasan Manna <manna@sharetrip.net>
2022-12-30 12:46:16 +0600

    breaking of ISP example

--- SOLID/InterfaceSegregationprinciple.swift
+++ SOLID/InterfaceSegregationprinciple.swift
@@ -11,3 +11,30 @@ import Foundation
  Interface Segregation Principle:
  Clients should not be forced to depend upon interfaces that they do not use.
  */
+
+//Breaing of ISP
+
+protocol Worker {
+    func work()
+    func eat()
+}
+
+class Human: Worker {
+    func work() {
+        print("Working")
+    }
+    func eat() {
+        print("Eating")
+    }
+}
+
+class Robot: Worker {
+    func work() {
+        print("Working")
+    }
+    
+    // Breaking of ISP
+    func eat() {
+        //Robot can't eat
+    }
+}
