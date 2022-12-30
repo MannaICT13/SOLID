ce590e4e1d953419ac7bf6d02358e073296501b5
Md Khaled Hasan Manna <manna@sharetrip.net>
2022-12-30 12:42:40 +0600

    define ISP

---
+++ SOLID/InterfaceSegregationprinciple.swift
@@ -0,0 +1,13 @@
+//
+//  InterfaceSegregationprinciple.swift
+//  SOLID
+//
+//  Created by Md Khaled Hasan Manna on 30/12/22.
+//
+
+import Foundation
+
+/*
+ Interface Segregation Principle:
+ Clients should not be forced to depend upon interfaces that they do not use.
+ */
