2fd76962ddb08e1a4920b799ed30c9c08ed0106d
Md Khaled Hasan Manna <manna@sharetrip.net>
2022-12-30 13:10:39 +0600

    example of DIP

--- SOLID/DependencyInversionPrinciple.swift
+++ SOLID/DependencyInversionPrinciple.swift
@@ -19,7 +19,7 @@ class Employee {
 }
 
 class Employer {
-    var employees: [Employee] // breaking the DIP
+    var employees: [Employee]
     
     init(employees: [Employee]) {
         self.employees = employees
@@ -34,3 +34,36 @@ class Employer {
 
 let instance = Employer(employees: [Employee]())
 //instance.manage()
+
+//****************************************** DIP Example ***********************************
+
+protocol Storage {
+    func save(string: String)
+}
+
+class FileSystemManager: Storage {
+    func save(string: String) {
+        // Open a file in read-mode
+        // Save the string in this file
+        // Close the file
+    }
+}
+class DatabaseManager: Storage {
+    func save(string: String) {
+        // Connect to the database
+        // Execute the query to save the string in a table
+        // Close the connection
+    }
+}
+
+class _Handler {
+    let storage: Storage
+    // Storage types
+    init(storage: Storage) {
+        self.storage = storage
+    }
+    
+    func handle(string: String) {
+        storage.save(string: string)
+    }
+}
