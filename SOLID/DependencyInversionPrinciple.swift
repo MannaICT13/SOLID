f3dc704e9a8bccfb4331e0f0e3834db6fc73a214
Md Khaled Hasan Manna <manna@sharetrip.net>
2022-12-30 13:07:02 +0600

    breaking of DIP

--- SOLID/DependencyInversionPrinciple.swift
+++ SOLID/DependencyInversionPrinciple.swift
@@ -12,3 +12,25 @@ import Foundation
  High-level modules should not depend on low-level modules both should depend on Abstractions.
  */
 
+class Employee {
+    func work() {
+        print("Working....")
+    }
+}
+
+class Employer {
+    var employees: [Employee] // breaking the DIP
+    
+    init(employees: [Employee]) {
+        self.employees = employees
+    }
+    
+    func manage() {
+        employees.forEach { employee in
+            employee.work()
+        }
+    }
+}
+
+let instance = Employer(employees: [Employee]())
+//instance.manage()
